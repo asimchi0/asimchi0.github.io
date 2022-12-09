fb6aec0db780549e5ffefab8913def0e51cc7c60
asimchi0 <102003781+asimchi0@users.noreply.github.com>
2022-12-09 18:28:01 -0500

    Update oneScript.js
    should include all added link with JS as well
    links that are not included as part of one page should work without any changes.

--- web115/scripts/oneScript.js
+++ web115/scripts/oneScript.js
@@ -1,15 +1,15 @@
 $(document).ready(function(e){
-    //code here
-	//alert('perform ready action here');
+
 	$('#onepage').html(pageDictionary["index.html"]);
 	
-	$('a').click(function(e){
-		e.preventDefault();
-		
-		//alert('perform click action here');
-		//alert($(this).attr('href'));
+	$(document).on('click', 'a', function(e) {
 		
 		let link = $(this).attr('href');
+		//alert(link);
+		if(pageDictionary[link]) {
+			e.preventDefault();
+		}
+		
 		$('#onepage').html(pageDictionary[link]);
 	});
 });
@@ -102,5 +102,588 @@ let pageDictionary = {
 				
 			</ul>
 		</section>
+`,
+"fizzbuzz/fizzbuzz0.html" :
+`		<section>			
+			<header>
+				<h1 id="greeting">Welcome to Fizz Buzz</h1>
+			</header>
+			
+			<form id="greetingForm" action="javascript:void(0)"  method="post">
+				<label for="firstName">First name:</label>
+				<input type="text" id="firstName" name="firstName" required><br>
+				<label for="middleInitial">Middle Initial:</label>
+				<input type="text" id="middleInitial" name="middleInitial" maxlength="1"><br>
+				<label for="lastName">Last name:</label>
+				<input type="text" id="lastName" name="lastName" required><br>
+				<label for="count">Count:</label>
+				<input type="number" id="count" name="count" value="200" min="10" max="1000" required><br>
+				<input type="submit" value="Submit">
+			</form>			
+			
+			<div id="loopHeader"></div>
+			<div id="loopSection"></div>
+			
+		</section>
+		<script>
+			function makeChanges()
+			{
+				changeGreeting();
+				changeLoop();
+			}
+			
+			function changeGreeting()
+			{
+				var greetingItem = document.getElementById('greeting'); // this is comment
+				var firstName = document.getElementById('firstName').value;
+				var middleInitial = document.getElementById('middleInitial').value;
+				var lastName = document.getElementById('lastName').value;
+				var newGreeting = "Welcome";
+				newGreeting = newGreeting.concat(" ", firstName);
+				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
+				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
+				greetingItem.textContent = newGreeting;
+				return false;
+			}
+			
+			function changeLoop()
+			{
+				var message = "Beep Boop";
+				var loopText = "Beep Boop";
+				
+				var loopHeaderItem = document.getElementById('loopHeader');								
+				var headerLoop = document.createElement("h2");
+				headerLoop.textContent = message;
+				loopHeaderItem.appendChild(headerLoop);								
+				
+				var loopSectionItem = document.getElementById('loopSection');
+				
+				var loopLimit = $('#count').val();
+				
+				var result="<ol>";
+				for (let i = 1; i <= loopLimit; i++) {
+					var addition;
+					if ( i % 2 == 0 ) { addition = "Even"; } 
+					else { addition = "Odd"; }
+					result = result.concat("<li>"," ",loopText," ",addition,"</li>");
+				}
+				result = result.concat("</ol>")
+				loopSectionItem.innerHTML = result;
+				return false;
+			}
+			
+			$('#greetingForm').on('submit', function(){
+				makeChanges();
+			});
+		</script>
+`,
+"fizzbuzz/fizzbuzz1.html" :
+`		<section>			
+			<header>
+				<h1 id="greeting">Welcome to Fizz Buzz</h1>
+			</header>
+
+			<form id="greetingForm" action="javascript:void(0)"  method="post">
+				<label for="firstName">First name:</label>
+				<input type="text" id="firstName" name="firstName" required><br>
+				<label for="middleInitial">Middle Initial:</label>
+				<input type="text" id="middleInitial" name="middleInitial" maxlength="1"><br>
+				<label for="lastName">Last name:</label>
+				<input type="text" id="lastName" name="lastName" required><br>
+				<label for="count">Count:</label>
+				<input type="number" id="count" name="count" value="200" min="10" max="1000" required><br>
+				<input type="submit" value="Submit">
+			</form>	
+
+			<div id="loopHeader"></div>
+			<div id="loopSection"></div>
+			
+		</section>
+		<script>
+			function makeChanges()
+			{
+				changeGreeting();
+				changeLoop();
+			}
+			
+			function changeGreeting()
+			{
+				var greetingItem = document.getElementById('greeting'); // this is comment
+				var firstName = document.getElementById('firstName').value;
+				var middleInitial = document.getElementById('middleInitial').value;
+				var lastName = document.getElementById('lastName').value;
+				var newGreeting = "Welcome";
+				newGreeting = newGreeting.concat(" ", firstName);
+				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
+				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
+				greetingItem.textContent = newGreeting;
+				return false;
+			}
+			
+			function changeLoop()
+			{
+				var message = "Beep";
+				var message3 = "Boop";
+				var message5 = "Zap";
+				
+				var loopHeaderItem = document.getElementById('loopHeader');								
+				var headerLoop = document.createElement("h2");
+				headerLoop.textContent = message;
+				loopHeaderItem.appendChild(headerLoop);	
+				
+				var loopSectionItem = document.getElementById('loopSection');
+				
+				var loopLimit = $('#count').val();
+				
+				var uList = document.createElement("ol");			
+				
+
+				for (let i = 1; i <= loopLimit; i++) 
+				{
+					var listItem = document.createElement("li");					
+					var addition;
+					if ( i % 3 == 0 && i % 5 == 0 ) { addition = message3 + " " + message5; }
+					else if ( i % 3 == 0 ) { addition = message3; } 
+					else if ( i % 5 == 0 ) { addition = message5; } 
+					else { addition = message; }
+					listItem.appendChild(document.createTextNode(addition));
+
+					uList.appendChild(listItem);
+				}
+
+				loopSectionItem.appendChild(uList);
+				return false;
+			}
+			
+			$('#greetingForm').on('submit', function(){
+				makeChanges();
+			});
+		</script>
+`,
+"fizzbuzz/fizzbuzz2.html" :
+`		<section>			
+			<header>
+				<h1 id="greeting">Welcome to Fizz Buzz</h1>
+			</header>
+
+			<form id="greetingForm" action="javascript:void(0)"  method="post">
+				<label for="firstName">First name:</label>
+				<input type="text" id="firstName" name="firstName" required><br>
+				<label for="middleInitial">Middle Initial:</label>
+				<input type="text" id="middleInitial" name="middleInitial" maxlength="1"><br>
+				<label for="lastName">Last name:</label>
+				<input type="text" id="lastName" name="lastName" required><br>
+				<label for="count">Count:</label>
+				<input type="number" id="count" name="count" value="200" min="10" max="1000" required><br>
+				<label>Enter number: word pairs</label><br>
+				<div class="numberText">
+					<input type="number" class="number" id="number1" name="number" value="3"> :
+					<input type="text" class="word" id="word1" name="word" value="Boop"><br>
+				</div>
+				<div class="numberText">
+					<input type="number" class="number" id="number2" name="number" value="5"> :
+					<input type="text" class="word" id="word2" name="word" value="Zap"><br>
+				</div>
+				<input type="submit" value="Submit">
+			</form>
+			
+			<div id="loopHeader"></div>
+			<div id="loopSection"></div>
+			
+		</section>
+		<script>
+			function makeChanges()
+			{
+				changeGreeting();
+				changeLoop();
+			}
+			
+			function changeGreeting()
+			{
+				var greetingItem = document.getElementById('greeting'); // this is comment
+				var firstName = document.getElementById('firstName').value;
+				var middleInitial = document.getElementById('middleInitial').value;
+				var lastName = document.getElementById('lastName').value;
+				var newGreeting = "Welcome";
+				newGreeting = newGreeting.concat(" ", firstName);
+				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
+				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
+				greetingItem.textContent = newGreeting;
+				return false;
+			}
+			
+			function changeLoop()
+			{
+				var message = "Beep";
+				var message3 = "Boop";
+				var message5 = "Zap";
+				
+				var loopHeaderItem = document.getElementById('loopHeader');								
+				var headerLoop = document.createElement("h2");
+				headerLoop.textContent = message;
+				loopHeaderItem.appendChild(headerLoop);			
+				
+				var loopSectionItem = document.getElementById('loopSection');
+				
+				var loopLimit = $('#count').val();
+				
+				var uList = document.createElement("ol");			
+				
+				
+				var x = $('#number1').val();
+				var y = $('#number2').val();
+				
+				for (let i = 1; i <= loopLimit; i++) 				
+				{
+
+					var listItem = document.createElement("li");					
+					var addition;
+					if ( checkDivision (i, x) && checkDivision (i, y) ) { addition = message3 + " " + message5; }
+					else if ( checkDivision (i, x) ) { addition = message3; } 
+					else if ( checkDivision (i, y) ) { addition = message5; } 
+					else { addition = message; }
+					listItem.appendChild(document.createTextNode(addition));
+
+					uList.appendChild(listItem);
+				}
+
+				loopSectionItem.appendChild(uList);				
+			}
+			function checkDivision(x, y)
+			{
+				return (x % y == 0);
+			}
+			
+			$('#greetingForm').on('submit', function(){
+				makeChanges();
+			});
+		</script>
+`,
+"fizzbuzz/fizzbuzz3.html" :
+`		<section>			
+			<header>
+				<h1 id="greeting">Welcome to Fizz Buzz</h1>
+			</header>
+
+			<form id="greetingForm" action="javascript:void(0)"  method="post">
+				<label for="firstName">First name:</label>
+				<input type="text" id="firstName" name="firstName" required><br>
+				<label for="middleInitial">Middle Initial:</label>
+				<input type="text" id="middleInitial" name="middleInitial" maxlength="1"><br>
+				<label for="lastName">Last name:</label>
+				<input type="text" id="lastName" name="lastName" required><br>
+				<label for="count">Count:</label>
+				<input type="number" id="count" name="count" value="200" min="10" max="1000" required><br>
+				<label>Enter number: word pairs</label><br>
+				<div class="numberText">
+					<input type="number" class="number" id="number1" name="number" value="3"> :
+					<input type="text" class="word" id="word1" name="word" value="Boop"><br>
+				</div>
+				<div class="numberText">
+					<input type="number" class="number" id="number2" name="number" value="5"> :
+					<input type="text" class="word" id="word2" name="word" value="Zap"><br>
+				</div>
+				<div class="numberText">
+					<input type="number" class="number" id="number3" name="number" value="7"> :
+					<input type="text" class="word" id="word3" name="word" value="BANG!"><br>
+				</div>
+				<input type="submit" value="Submit">
+			</form>
+
+			<div id="loopHeader"></div>
+			<div id="loopSection"></div>
+			
+		</section>
+		<script>
+			function makeChanges()
+			{
+				changeGreeting();
+				changeLoop();
+			}
+			
+			function changeGreeting()
+			{
+				var greetingItem = document.getElementById('greeting'); // this is comment
+				var firstName = document.getElementById('firstName').value;
+				var middleInitial = document.getElementById('middleInitial').value;
+				var lastName = document.getElementById('lastName').value;
+				var newGreeting = "Welcome";
+				newGreeting = newGreeting.concat(" ", firstName);
+				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
+				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
+				greetingItem.textContent = newGreeting;
+				return false;
+			}
+			
+			function changeLoop()
+			{
+				var defaultMessage = "Beep";
+				
+				var divisorToMessage = {3 : "Boop", 5 : "Zap", 7 : "BANG!"}; // default
+				
+				var loopHeaderItem = document.getElementById('loopHeader');								
+				var headerLoop = document.createElement("h2");
+				headerLoop.textContent = defaultMessage;
+				loopHeaderItem.appendChild(headerLoop);				
+				
+				var loopSectionItem = document.getElementById('loopSection');
+				
+				var loopLimit = $('#count').val();
+				
+				var uList = document.createElement("ol");			
+				
+				
+				var x = $('#number1').val();
+				var y = $('#number2').val();
+				var z = $('#number3').val();
+				
+				divisorToMessage = { [x] : "Boop", [y] : "Zap", [z] : "BANG!"}; 
+				
+				for (let i = 1; i <= loopLimit; i++) 				
+				{
+					var listItem = document.createElement("li");					
+					var addition = "";
+					for (let [key, value] of Object.entries(divisorToMessage)) {
+						 if (checkDivision (i, key)) {
+							addition = addition.concat(" ", value);
+						}
+					}
+					if (addition === "") {
+						addition = defaultMessage;
+					}
+					/*if ( checkDivision (i, x) && checkDivision (i, y) ) { addition = message3 + " " + message5; }
+					else if ( checkDivision (i, x) ) { addition = message3; } 
+					else if ( checkDivision (i, y) ) { addition = message5; } 
+					else { addition = message; }*/
+					listItem.appendChild(document.createTextNode(addition));
+
+					uList.appendChild(listItem);
+				}
+
+				loopSectionItem.appendChild(uList);				
+			}
+			function checkDivision(x, y)
+			{
+				return (x % y == 0);
+			}
+			
+			$('#greetingForm').on('submit', function(){
+				makeChanges();
+			});
+		</script>
+`,
+"fizzbuzz/fizzbuzz4.html" :
+`		<section>			
+			<header>
+				<h1 id="greeting">Welcome to Fizz Buzz</h1>
+			</header>
+			
+			<form id="greetingForm" action="javascript:void(0)"  method="post">
+			<div class="formAlign">
+				<label for="firstName">First name:</label>
+				<input type="text" id="firstName" name="firstName" required><br>
+				<label for="middleInitial">Middle Initial:</label>
+				<input type="text" id="middleInitial" name="middleInitial" maxlength="1"><br>
+				<label for="lastName">Last name:</label>
+				<input type="text" id="lastName" name="lastName" required><br>
+				<label for="count">Count:</label>
+				<input type="number" id="count" name="count" value="200" min="10" max="1000" required><br>
+			</div>
+				<label for="defaultWord">Default word :</label>
+				<input type="text" id="defaultWord" name="defaultWord"><br>
+			
+				<label>Enter number: word pairs</label><br>
+				<div class="numberText">
+					<input type="number" class="number" id="number1" name="number" value="3"> :
+					<input type="text" class="word" id="word1" name="word"><br>
+				</div>
+				<div class="numberText">
+					<input type="number" class="number" id="number2" name="number" value="5"> :
+					<input type="text" class="word" id="word2" name="word"><br>
+				</div>
+				<div class="numberText">
+					<input type="number" class="number" id="number3" name="number" value="7"> :
+					<input type="text" class="word" id="word3" name="word"><br>
+				</div>				
+				<input type="submit" value="Submit">
+				<input type="button" id="addMore" value="Add more">
+				<input type="button" id="remove" value="Remove">
+				<input type="checkbox" id="useWord" checked>
+				<label for="useWord">Use default word</label>
+				
+			</form>	
+
+			<div id="loopHeader"></div>
+			<div id="loopSection"></div>
+			
+		</section>
+		<script>
+			const words = ["Boop", "Zap", "Bang", "Test", "Taste", "Food", "Bee", "Dog", "Cat", "Bird", "I", "Run", "Out", "Of", "Ideas", "Nope", "None", "Nothing", "Still", "Zero", "Do", "Not", "Expect", "More", "Words", "Wait", "Minute", "Something", "Anything", "Everything", "Stop", "Clicking!", "Nooo", "Sdfghdf"];
+			
+			const defaultMessage = "Beep";
+			
+			const defaultCount = 125;
+		
+			function makeChanges()
+			{
+				changeGreeting();
+				changeLoop();
+			}
+			
+			function changeGreeting()
+			{
+				let greetingItem = document.getElementById('greeting'); // this is comment
+				let firstName = document.getElementById('firstName').value;
+				let middleInitial = document.getElementById('middleInitial').value;
+				let lastName = document.getElementById('lastName').value;
+				let newGreeting = "Welcome";
+				newGreeting = newGreeting.concat(" ", firstName);
+				if (middleInitial !== "") { newGreeting = newGreeting.concat(" ", middleInitial, "."); }
+				newGreeting = newGreeting.concat(" ", lastName, " ","to Fizz Buzz");				
+				greetingItem.textContent = newGreeting;
+				return false;
+			}
+			
+			function collectNumberWordPairs() {
+				let numberWordPairs = {};
+				
+				
+				$('div[class=numberText]').each(function() { // select all element with numberText workaround from direct class selector
+				
+					let number = Number($(this).find('.number').val()); // convert to number, '' converts into 0
+					
+					if (number) { // have number 
+						
+						let word = $(this).find('.word').val(); // collect word
+						
+						if (word) { // have word
+							numberWordPairs[number] = word; // add new pair
+						}
+					}
+				})
+				
+				/*
+				//DEBUG
+				for (let [key, value] of Object.entries(numberWordPairs)) {
+					alert(key + " : " + value);
+				}
+				*/
+				
+				return numberWordPairs;
+			}
+			
+			function changeLoop()
+			{			
+				let message =  $('#defaultWord').val();
+				
+				let useWord = $('#useWord').prop('checked');				
+				
+				//let divisorToMessage = {3 : "Boop", 5 : "Zap", 7 : "BANG!"}; // default
+				
+				let divisorToMessage = collectNumberWordPairs(); // collect values;
+				
+				var loopHeaderItem = document.getElementById('loopHeader');								
+				var headerLoop = document.createElement("h2");
+				headerLoop.textContent = message;
+				loopHeaderItem.appendChild(headerLoop);					
+				
+				let loopSectionItem = document.getElementById('loopSection');
+				
+				let loopLimit = $('#count').val();				
+				
+				let uList = document.createElement("ol");			
+				
+				for (let i = 1; i <= loopLimit; i++) 				
+				{
+					let listItem = document.createElement("li");					
+					let addition = "";
+					for (let [key, value] of Object.entries(divisorToMessage)) {
+						 if (checkDivision (i, key)) {
+							addition = addition.concat(" ", value);
+						}
+					}
+					if (addition === "") {
+						if (useWord) {
+							addition = message;
+						}
+						else {
+							addition = i;
+						}
+					}
+					
+					listItem.appendChild(document.createTextNode(addition));
+
+					uList.appendChild(listItem);
+				}
+				loopSectionItem.innerHTML = ''; // clear html to add new list
+				loopSectionItem.appendChild(uList);				
+			}
+			
+			function checkDivision(x, y)
+			{
+				return (x % y == 0);
+			}
+			
+			function preFillFields() {
+				
+				$('#count').attr('value', defaultCount);
+			
+				$defaultWord = $('#defaultWord');
+				$defaultWord.attr('value', defaultMessage);
+				
+				$('div[class=numberText]').each(function() {
+					let $index = $(this).index("div");	
+					
+					$(this).find('.word').attr('value',words[$index - 1]);
+				})
+				
+			}
+
+			$('#greetingForm').on('submit', function(){
+				makeChanges();
+			});
+			
+			$('#addMore').on('click', function() {
+				
+				let $element = $('div[class=numberText]').last();
+				let $index = $element.index("div"); // index of previous last element			
+				
+				let $d = $('<div>');
+				$d.attr('class','numberText');
+				
+				let $b = $('<br>');
+				
+				let $s = document.createTextNode(" : ");
+			
+				let $n = $('<input>');
+				$n.attr('type','number');
+				$n.attr('class','number');
+				$n.attr('id','number'+$index);
+				$n.attr('name','number');
+				
+				let $t = $('<input>');
+				$t.attr('type','text');
+				$t.attr('class','word');
+				$t.attr('id','word'+$index);
+				$t.attr('name','word');
+
+				if (words[$index]) {
+					$t.attr('value',words[$index]);
+				}
+				
+				$d.append($n);
+				$d.append($s);
+				$d.append($t);
+				$d.append($b);
+				
+				$element.after($d);				
+			})
+			
+			$('#remove').on('click', function() {
+				$('div[class=numberText]').slice(3).last().remove();
+			});
+			preFillFields();
+			
+		</script>
 `
 }
