1f4e52b881b58f9df99d98dbc9ea94b76066368e
asimchi0 <102003781+asimchi0@users.noreply.github.com>
2023-04-02 02:58:10 -0400

    Create test.php
    +file to test PHP

---
+++ web250/test.php
@@ -0,0 +1,10 @@
+<?php
+$sOutputString = "<hr/>";
+
+for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+{
+		$sOutputString .= $iCounter . ") Alexei got PHP running on my computer!! ";
+}
+
+echo ("<h5>" . $sOutputString . "</h5><hr/>");
+?>
