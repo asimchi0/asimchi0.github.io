5685de1ebc4de6c1eed04e6e3b6a5d6ea29c0924
asimchi0 <102003781+asimchi0@users.noreply.github.com>
2023-05-02 11:07:45 -0400

    Clean up JS scripts
    - removed unused script
    + added dynamic title change
    + add jQuery library to pages.

--- web215/scripts/addNavigation.js
+++ web215/scripts/addNavigation.js
@@ -1,40 +1,45 @@
-var pages = { // declaring navigation pages; key - name on Page : value - link to page
+let pages = { // declaring navigation pages; key - name on Page : value - link to page
 	Home : "index.html",
 	Introduction : "introduction.html",
-	Contract : "contract.html"//,
-	//Brand : "brand.html",
+	Contract : "contract.html",
+	Brand : "brand.html"//,
 	//TOC : "toc.html",
-	//"One Page" : "onepage.html"
 }
-var leveNavlOffset = "";
+
+let fullName = 'Alexei Simchiv-Mattson';
+let course = 'WEB215';
+let siteName = 'Advanced Snake';
+let composedName = `${course} | ${fullName}'s ${siteName}`
+
+let levelNavOffset = "";
 
 function addLogo(navigation) // adding logo (in this case just test with link to starting page)
 {
-	//var navigation = document.getElementById("navigation"); // get element with navigation id
+	//let navigation = document.getElementById("navigation"); // get element with navigation id
 	
-	var logo = document.createElement("div"); // create div element
-	var a = document.createElement("a");			
-	var logoLink  = leveNavlOffset + "index.html";
+	let logo = document.createElement("div"); // create div element
+	let a = document.createElement("a");			
+	let logoLink  = levelNavOffset + "index.html";
 
 
 	a.setAttribute("href", logoLink); // setting ling to index page
-	var headerTemp = document.createElement("h1");
-	headerTemp.appendChild(document.createTextNode("Alexei's WEB215"));
-	a.appendChild(headerTemp); // adding created elelemnts together
+	let headerTemp = document.createElement("h1");
+	headerTemp.appendChild(document.createTextNode(`${composedName}`));
+	a.appendChild(headerTemp); // adding created elements together
 	logo.appendChild(a);
 	navigation.appendChild(logo);
 }
 function addNavigationBar(navigation)
 {
-	//var navigation = document.getElementById("navigation");
-	var navUl = document.createElement("ul");
-	for(var key in pages)
+	//let navigation = document.getElementById("navigation");
+	let navUl = document.createElement("ul");
+	for(let key in pages)
 	{
-		var value = pages[key];				
-		var navLi = document.createElement("li")
-		var a = document.createElement("a");
+		let value = pages[key];				
+		let navLi = document.createElement("li")
+		let a = document.createElement("a");
 		
-		value = leveNavlOffset + value;
+		value = levelNavOffset + value;
 
 		a.setAttribute("href", value);
 		
@@ -46,17 +51,37 @@ function addNavigationBar(navigation)
 	}			
 	navigation.appendChild(navUl);
 }
+function changePageTitle()
+{
+	const currentUrl = new URL(window.location.href);
+	let pathname = currentUrl.pathname;
+	let pathnameParsed = pathname.split('/');
+	let lastPathname = pathnameParsed.at(-1).toLowerCase();
+	let titlePart = ''
+	for (let key in pages)
+	{
+		let value = pages[key];		
+		if (lastPathname === value)
+		{			
+			titlePart = key;
+		}
+	}
+	document.title = `${composedName} | ${titlePart}`;
+}
+
 function addNavigation(level = 0)
 {
-	for (var i = 0; i < level; i++)
+	for (let i = 0; i < level; i++)
 	{
-		leveNavlOffset = "../" + leveNavlOffset;
+		levelNavOffset = "../" + levelNavOffset;
 	}	
 	
-	var body = document.body;
-	var headerNav = document.createElement("header");
-	var navigation = document.createElement("nav");
+	let body = document.body;
+	let headerNav = document.createElement("header");
+	let navigation = document.createElement("nav");
 	
+
+	changePageTitle()
 	addLogo(navigation, level);
 	addNavigationBar(navigation, level);
 	
