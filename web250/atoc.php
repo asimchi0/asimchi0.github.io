e460f6fa0384b7a55c21091d70bf55f7eaa2a86a
asimchi0 <102003781+asimchi0@users.noreply.github.com>
2023-05-03 13:49:55 -0400

    atoc fix
    + added link to HTML/PHP versions of website
    ! switched to relative paths for webpage within same domain

--- web250/atoc.php
+++ web250/atoc.php
@@ -1,6 +1,7 @@
 <h2>Table Of Contents for additional pages (ATOC)</h2>
 <p>Note: Links open in new tab/windows and lead outside of regular website</p>
 <ul>
-	<li><a target="_blank" href="http://amphibiousshark.22web.org/superduper_php/">Dynamic PHP page</a></li>
+	<li><a target="_blank" href="index.html">HTML version of website</a></li>
+	<li><a target="_blank" href="./superduper_php/">Dynamic PHP page</a></li>
 	<!-- <li><a target="_blank" href="http://amphibiousshark.22web.org/joy/">Joy of PHP website</a></li> -->
 </ul>	
