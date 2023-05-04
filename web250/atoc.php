36a9bb5d5b6538c7b90ee40d4702e4b94eaf7e3d
asimchi0 <102003781+asimchi0@users.noreply.github.com>
2023-05-04 13:05:30 -0400

    added static template
    + static HTML template added.
    + added link to static template to ATOC

--- web250/atoc.php
+++ web250/atoc.php
@@ -2,6 +2,7 @@
 <p>Note: Links open in new tab/windows and lead outside of regular website</p>
 <ul>
 	<li><a target="_blank" href="index.html">HTML version of website</a></li>
-	<li><a target="_blank" href="./superduper_php/">Dynamic PHP page</a></li>
+	<li><a target="_blank" href="./superduper_php/index.php">Dynamic PHP page</a></li>
+	<li><a target="_blank" href="./superduper_static/index.htm">Static HTML page</a></li>
 	<!-- <li><a target="_blank" href="./joy/">Joy of PHP website</a></li> -->
 </ul>	
